dc2415032bf57293e21b5b9f1333bd101eb271f4
apfesta <andy_festa@yahoo.com>
2018-10-23 21:55:23 -0400

    initial revision

---
+++ src/main/java/net/mixednutz/app/server/MixednutzAppServerApplication.java
@@ -0,0 +1,12 @@
+package net.mixednutz.app.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixednutzAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixednutzAppServerApplication.class, args);
+	}
+}
