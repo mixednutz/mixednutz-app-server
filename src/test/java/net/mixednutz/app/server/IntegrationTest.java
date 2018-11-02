84df219cff01a2b7d729cc1ca05414a896c7feae
apfesta <andy_festa@yahoo.com>
2018-11-02 14:11:05 -0400

    IntegrationTest

---
+++ src/test/java/net/mixednutz/app/server/IntegrationTest.java
@@ -0,0 +1,7 @@
+/**
+ * user this interface to flag unit tests as Integration Tests
+ * Usage: @Category(IntegrationTest.class)
+ * @see org.junit.experimental.categories.Category
+ */
+public interface IntegrationTest {
+}
