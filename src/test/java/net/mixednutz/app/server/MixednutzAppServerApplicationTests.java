ae681336eb007efbb16e4b54f37e2c978f0886a1
apfesta <andy_festa@yahoo.com>
2019-06-07 14:48:10 -0400

    containerized

--- src/test/java/net/mixednutz/app/server/MixednutzAppServerApplicationTests.java
+++ src/test/java/net/mixednutz/app/server/MixednutzAppServerApplicationTests.java
@@ -1,11 +1,13 @@
 package net.mixednutz.app.server;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@Category(IntegrationTest.class)
 @SpringBootTest
 public class MixednutzAppServerApplicationTests {
 
