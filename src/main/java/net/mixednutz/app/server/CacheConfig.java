55b41ccf44c1bd541606b1d2977c54c66cd7119b
apfesta <andy_festa@yahoo.com>
2021-01-18 14:49:13 -0500

    added caching

---
+++ src/main/java/net/mixednutz/app/server/CacheConfig.java
@@ -0,0 +1,11 @@
+package net.mixednutz.app.server;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+	
+}
