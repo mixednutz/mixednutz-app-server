06cb413daafdbf916255fbc93be884c2478271b9
apfesta <andy_festa@yahoo.com>
2021-02-21 20:19:22 -0500

    got docker image ready, disabled int tests

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -2,8 +2,8 @@
 
 
 # Set Default Active Profiles
-if [[ -z "$SPRING_OPTS" ]]; then
-	export SPRING_OPTS='--spring.profiles.active=jpa-prod,uploads-dev,mixednutz_base,twitter,aws-local'
+if [ -z "$SPRING_OPTS" ]; then
+	export SPRING_OPTS='--spring.profiles.active=ssl'
 fi
 
 echo 'Sleeping 30 seconds to ensure DB is started'
@@ -12,4 +12,4 @@ sleep 30s
 java \
 -Djava.security.egd=file:/dev/./urandom \
 -Drun.addResources="false" \
--jar /app.jar ${SPRING_OPTS}
+-jar /@project.build.finalName@.jar ${SPRING_OPTS}
